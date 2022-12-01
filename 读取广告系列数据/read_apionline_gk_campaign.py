c86841d12506c1f9d4f084b15e88147cd1a0c8c1
Ume | (跨境)电商AI服务 <newswithsnow.2022@gmail.com>
2022-12-01 18:08:48 +0800

    v.0.1-新增了订单表数据，高度汇总表数据，同时修正campaign和campaign_report表的sql以重新拉取s更易处理的广告系列数据

--- 读取广告系列数据/read_apionline_gk_campaign.py
+++ 读取广告系列数据/read_apionline_gk_campaign.py
@@ -4,7 +4,7 @@
 @Author:UmeAI
 @File:read_apionline_gk_campaign.py
 @Time:2022/11/26 15:18
-@Read: 读取广告系列相关数据；读取的是优化从产品出发，投放出来的广告系列数据；
+@Read: 读取广告系列相关数据；读取的是优化从产品出发，投放出来的广告系列数据； 只选择了投放在FB平台的广告系列
 """
 import pickle
 import pymysql
@@ -23,38 +23,18 @@ def readdatafromdatabase(sql):
 
 
 if __name__ == '__main__':
-    sql_campaign = """select * from gk_campaign"""
+    sql_campaign = """select 
+    id,account_id, campaign_id,campaign_name, 
+    ader_id,chooser_id,chooser_name,designer_id,designer_name,
+    goods_id as '产品ID', goods_name as '产品名', category,product_id as '商品ID', product_name as '商品名',
+    start_time
+    from gk_campaign where platform = 'facebook' """
     dataDF = readdatafromdatabase(sql_campaign)
-    # with open('giikinadcampaign.pick', 'wb') as f:
-    #     pickle.dump(dataDF, f)
-
+    with open('giikinadcampaign.pick', 'wb') as f:
+        pickle.dump(dataDF, f)
 
 sql = """
 CREATE TABLE `gk_campaign` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '系列id索引',
-  `account_id` varchar(50) DEFAULT NULL COMMENT '广告账号id',
-  `bid_amount` decimal(19,2) DEFAULT NULL COMMENT '竞价金额',
-  `bid_strategy` varchar(50) DEFAULT NULL COMMENT '竞价策略',
-  `budget_type` varchar(50) DEFAULT NULL COMMENT '预算类型',
-  `campaign_id` varchar(50) DEFAULT NULL COMMENT '系列id',
-  `campaign_name` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '系列名称',
-  `daily_budget` decimal(19,2) DEFAULT NULL COMMENT '日预算',
-  `end_time` datetime DEFAULT NULL COMMENT '结束时间',
-  `ader_id` int(11) DEFAULT '0' COMMENT '优化师id',
-  `ader_name` varchar(50) DEFAULT NULL COMMENT '优化师名称',
-  `area_id` int(11) DEFAULT '0' COMMENT '团队id',
-  `area_name` varchar(50) DEFAULT NULL COMMENT '团队名称',
-  `category` varchar(255) DEFAULT NULL COMMENT '产品分类名称',
-  `category_id` int(11) DEFAULT '0' COMMENT '产品分类id',
-  `chooser_id` int(11) DEFAULT '0' COMMENT '选品id',
-  `chooser_name` varchar(50) DEFAULT NULL COMMENT '选品名称',
-  `coll_id` int(11) DEFAULT '0' COMMENT '站点id',
-  `currency_id` int(11) DEFAULT '0' COMMENT '老币种id',
-  `designer_id` int(11) DEFAULT '0' COMMENT '设计id',
-  `designer_name` varchar(50) DEFAULT NULL COMMENT '设计名称',
-  `domain` varchar(250) DEFAULT NULL COMMENT '域名',
-  `goods_id` int(11) DEFAULT '0' COMMENT '产品id',
-  `goods_name` varchar(255) DEFAULT NULL COMMENT '产品名称',
   `is_change` bit(1) DEFAULT b'0' COMMENT '是否改派 0不是,1 是',
   `is_test_product` bit(1) DEFAULT b'0' COMMENT '/是否测品 ，0 不是， 1 是',
   `low_price` bit(1) DEFAULT b'0' COMMENT '是否低价  0不是 ，1 是',
@@ -81,10 +61,35 @@ CREATE TABLE `gk_campaign` (
   `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录-更新时间',
   `target_cpa` decimal(19,2) DEFAULT NULL COMMENT '目标出价金额',
   `type` varchar(50) DEFAULT NULL COMMENT '系列类型/系列目标',
+
+
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '系列id索引',
+  `account_id` varchar(50) DEFAULT NULL COMMENT '广告账号id',
+  `bid_amount` decimal(19,2) DEFAULT NULL COMMENT '竞价金额',  # 都是 None 就不选择了,
+  `bid_strategy` varchar(50) DEFAULT NULL COMMENT '竞价策略',  # 也都是 None 就不选择了,
+  `budget_type` varchar(50) DEFAULT NULL COMMENT '预算类型',  # 都是 none 就不选择了,
+  `campaign_id` varchar(50) DEFAULT NULL COMMENT '系列id',
+  `campaign_name` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '系列名称',
+  `daily_budget` decimal(19,2) DEFAULT NULL COMMENT '日预算',   # none,
+  `end_time` datetime DEFAULT NULL COMMENT '结束时间', 
+  `ader_id` int(11) DEFAULT '0' COMMENT '优化师id',
+  `ader_name` varchar(50) DEFAULT NULL COMMENT '优化师名称',
+  `area_id` int(11) DEFAULT '0' COMMENT '团队id',
+  `area_name` varchar(50) DEFAULT NULL COMMENT '团队名称',
+  `category` varchar(255) DEFAULT NULL COMMENT '产品分类名称',
+  `category_id` int(11) DEFAULT '0' COMMENT '产品分类id',
+  `chooser_id` int(11) DEFAULT '0' COMMENT '选品id',
+  `chooser_name` varchar(50) DEFAULT NULL COMMENT '选品名称',
+  `coll_id` int(11) DEFAULT '0' COMMENT '站点id',
+  `currency_id` int(11) DEFAULT '0' COMMENT '老币种id',
+  `designer_id` int(11) DEFAULT '0' COMMENT '设计id',
+  `designer_name` varchar(50) DEFAULT NULL COMMENT '设计名称',
+  `domain` varchar(250) DEFAULT NULL COMMENT '域名',
+  `goods_id` int(11) DEFAULT '0' COMMENT '产品id',
+  `goods_name` varchar(255) DEFAULT NULL COMMENT '产品名称',
+
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK1bjaf7nkamvo4a39qt8xxaket` (`campaign_id`,`platform`),
   KEY `IDXnswbc9m1lod02nbpivf3c3267` (`account_id`,`platform`)
 ) ENGINE=InnoDB AUTO_INCREMENT=564618 DEFAULT CHARSET=utf8;
-
-
 """
--- 读取广告系列数据/读取所有商品对应的广告系列数据.py
+++ 读取广告系列数据/读取所有商品对应的广告系列数据.py
@@ -21,6 +21,19 @@ with open('giikinadcampaign_report.pick', 'rb') as f:
     giikin_campaign_report = pickle.load(f)
 # campaign_id__report_groups = giikin_campaign_report.groupby('gk_campaign_id').groups
 # campaign_id__report_groups = giikin_campaign_report.groupby('gk_campaign_id')
+# todo 给字段重命名
+#
+# todo 删除一些字段
+
+
+
+# todo 对广告campaign的report按照id做汇总
+giikin_campaign_report_groups = giikin_campaign_report.groupby('gk_campaign_id').groups
+for key, lineindex in giikin_campaign_report_groups.items():
+    if key == 2:
+        res = giikin_campaign_report.loc[lineindex]
+        res['date'] = res.to_datetime(res['date'])
+
 
 # 优化师投放出来的广告和广告的report数据信息的关联
 context = giikin_campaign.set_index('id').join(giikin_campaign_report.set_index('gk_campaign_id'))
